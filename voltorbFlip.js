23b48b38b36c2bdb47ff2a21ebd2ffd9c8fc5384
Akronoskos <33528348+ThomasDearth@users.noreply.github.com>
2020-02-16 06:53:39 -0500

    Detects coordinates of clicked squares

--- voltorbFlip.js
+++ voltorbFlip.js
@@ -1,12 +1,31 @@
 //Setup
-/* @pjs preload="resources/background/boardBackground.png" */
+/* @pjs preload="resources/background/boardBackground.png"*/
 
 var boardBackground = loadImage("resources/background/boardBackground.png");
-var gameElement = document.getElementById("game");
-
+boardBackground.width *= 2;
+boardBackground.height *= 2;
 width = boardBackground.width;
 height = boardBackground.height;
 
+var dispText = "";
+
 draw = function() {
   image(boardBackground, 0, 0);
+  text(dispText, 400, 40);
+}
+
+mouseClicked = function() {
+  //Detect which tile is clicked and store it in dispText.
+  var xSquare, ySquare;
+  if((mouseX - 24) % 64 > 44) {
+    xSquare = null;
+  } else {
+    xSquare = floor((mouseX - 24) / 64);
+  }
+  if((mouseY - 22) % 64 > 44) {
+    ySquare = null;
+  } else {
+    ySquare = floor((mouseY - 22) / 64);
+  }
+  dispText = xSquare + " " + ySquare;
 }
