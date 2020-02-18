9b495a86f89cfeac13097e6f1214e127961a1ed8
Akronoskos <33528348+ThomasDearth@users.noreply.github.com>
2020-02-18 14:59:58 -0500

    boardTile now initializes whole board model

--- boardTile.js
+++ boardTile.js
@@ -1,14 +1,61 @@
-var tiles = [];
+//Variable storing the logical components of the board.
+var tiles = [[], [], [], [], []];
+
+var newTile = function(score) {
+  var ret = {
+    score: score,
+    flipped: false
+  }
+  return ret;
+};
 
 //Initialize the empty board
-for(var row = 0; row < gridCoords.rows; row++) {
-  var columns = [];
-  for(var column = 0; column < gridCoords.columns; column++) {
-    var tile = {
-      score: 1,
-      flipped: false
-    };
-    columns.push(tile);
+var initializeTiles = function() {
+  tiles = [[], [], [], [], []];
+
+  var info = getLevelContents(1);
+  var score = Math.pow(2, info[0]) * Math.pow(3, info[1]);
+
+  //Create 2s
+  while(info[0] > 0) {
+    var randColumn = Math.floor(5 * Math.random());
+    var randRow = Math.floor(5 * Math.random());
+    if(tiles[randRow][randColumn] === undefined) {
+      tiles[randRow][randColumn] = newTile(2);
+      info[0]--;
+    }
   }
-  tiles.push(columns);
+
+  //Create 3s
+  while(info[1] > 0) {
+    var randColumn = Math.floor(5 * Math.random());
+    var randRow = Math.floor(5 * Math.random());
+    if(tiles[randRow][randColumn] === undefined) {
+      tiles[randRow][randColumn] = newTile(3);
+      info[1]--;
+    }
+  }
+
+  //Create 0s
+  while(info[2] > 0) {
+    var randColumn = Math.floor(5 * Math.random());
+    var randRow = Math.floor(5 * Math.random());
+    if(tiles[randRow][randColumn] === undefined) {
+      tiles[randRow][randColumn] = newTile(0);
+      info[2]--;
+    }
+  }
+
+  //Create 1s
+  for(var row = 0; row < gridCoords.rows; row++) {
+    for(var col = 0; col < gridCoords.columns; col++) {
+      if(tiles[row][col] === undefined) {
+        tiles[row][col] = newTile(1);
+      }
+    }
+  }
+
+  return score;
 }
+
+var score = initializeTiles();
--- voltorbFlip.js
+++ voltorbFlip.js
@@ -9,41 +9,40 @@ var tile1 = loadImage("resources/tiles/tile1.png");
 width = boardBackground.width;
 height = boardBackground.height;
 
-var tileSelected = [null, null];
-
-draw = function() {
+setup = function() {
   image(boardBackground, 0, 0);
-  text(tileSelected, 400, 40);
-  image(tile1,
-    gridCoords.xOffset + 64 * tileSelected[0],
-    gridCoords.yOffset + 64 * tileSelected[1],
-    2 * tile1.width,
-    2 * tile1.height);
-  text(getLevelContents(1), 400, 60);
 }
 
 mouseClicked = function() {
-  getSelectedTile();
+  //Draws tiles where the mouse is clicked
+  //TODO: fix drawing when selections are made between tiles
+  var tileSelected = getSelectedTile();
+  if(tileSelected.x !== null & tileSelected.y !== null) {
+    image(tile1,
+      gridCoords.xOffset + 64 * tileSelected.column,
+      gridCoords.yOffset + 64 * tileSelected.row,
+      2 * tile1.width, 2 * tile1.height);
+  }
 }
 
-//Detect which tile is clicked and store it in tileSelected.
+/*Detect which tile is clicked and return it in an object with the row and
+  column of the tile.*/
 var getSelectedTile = function() {
-    var xSquare, ySquare;
-    if(((mouseX - gridCoords.xOffset) % 64) > 44) {
-      xSquare = null;
-    } else {
-      xSquare = floor((mouseX - gridCoords.xOffset) / 64);
-      if(xSquare < 0 | xSquare >= gridCoords.columns) {
-        xSquare = null;
-      }
+  var coords = {
+    column: null,
+    row: null
+  };
+  if(!(((mouseX - gridCoords.xOffset) % 64) > 44)) {
+    coords.column = Math.floor((mouseX - gridCoords.xOffset) / 64);
+    if(coords.column < 0 | coords.column >= gridCoords.columns) {
+      coords.column = null;
     }
-    if((mouseY - gridCoords.yOffset) % 64 > 44) {
-      ySquare = null;
-    } else {
-      ySquare = floor((mouseY - gridCoords.yOffset) / 64);
-      if(ySquare < 0 | ySquare >= gridCoords.rows) {
-        ySquare = null;
-      }
   }
-  tileSelected = [xSquare, ySquare];
+  if(!(((mouseY - gridCoords.YOffset) % 64) > 44)) {
+    coords.row = Math.floor((mouseY - gridCoords.yOffset) / 64);
+    if(coords.row < 0 | coords.row >= gridCoords.rows) {
+      coords.row = null;
+    }
+  }
+  return coords;
 }
